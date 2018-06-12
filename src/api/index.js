8d2194503c745e9725df8b3af757dcf4ef3f1ef3
Ian Hung <yienh@google.com>
2018-06-12 17:56:58 +0800

    add product & more product API

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,10 @@ import facets from './facets';
 import findCurrencyRatio from './findCurrencyRatio';
 import goodsLikeCount from './goodsLikeCount';
 import searchGoodsResult from './searchGoodsResult';
+import moreProducts_0 from '../mock/more_products_0.json';
+import moreProducts_1 from '../mock/more_products_1.json';
+import products from '../mock/products.json';
+
 export default ({ config, db }) => {
 	let api = Router();
 
@@ -19,6 +23,29 @@ export default ({ config, db }) => {
 
 	// 获取搜索结果(GET方法)
 	api.use('/search', searchGoodsResult({ config, db}));
+
+	// GET Product
+	api.get('/products', (req, res) => {
+		res.json(products);
+	})
+
+	// Get More Product with queryStr [moreItemsIndex]
+	api.get('/more_products', (req, res) => {
+		switch(req.query.moreItemsIndex) {
+			case '0':
+				res.json(moreProducts_0);
+				break;
+			case '1':
+				res.json(moreProducts_1);
+				break;
+			default:
+				res.json({
+					"items": [],
+					"hasMorePages": false,
+				});
+				break;
+		}
+	})
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
